f26b8cf179d74509ab2bbc2f16f99bc152e8b8ac
telmanagababov <telman@backbase.com>
2021-12-13 17:59:14 +0100

    steps 1-3

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
-    MatCardModule,
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
