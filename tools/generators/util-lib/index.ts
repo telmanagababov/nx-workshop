d022c0fe34a4eaff476fe6dca84ceac0837432d9
telmanagababov <telman@backbase.com>
2021-12-14 16:34:10 +0100

    step 13.2

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -2,7 +2,9 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  const libraryDestination = `${schema.directory}/util-${schema.name}`;
+  const libraryTags = `type:util,scope:${schema.directory}`;
+  await libraryGenerator(tree, { name: libraryDestination, tags: libraryTags });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
